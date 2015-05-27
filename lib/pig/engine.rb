f63198e1bab833f3a976659f1c60545cbece1e94
Greg Woodcock <greg@yoomee.com>
2015-05-27 10:54:02 +0100

    Dont isolate the engine

--- lib/pig/engine.rb
+++ lib/pig/engine.rb
@@ -1,18 +1,11 @@
 module Pig
   class Engine < ::Rails::Engine
-    isolate_namespace Pig
 
     config.to_prepare do
       Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
         require_dependency(c)
       end
     end
-
-    # config.to_prepare do
-    #   Dir.glob(Rails.root + "app/helpers/**/*_helper*.rb").each do |c|
-    #     require_dependency(c)
-    #   end
-    # end
 
     config.nested_permalinks = true
     config.tags_feature = false
