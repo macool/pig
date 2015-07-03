984e4009dde91499cdb63f66dc58df812bc28855
Greg Woodcock <greg@yoomee.com>
2015-07-03 15:12:10 +0100

    Add persona controller tests for the index action

--- spec/controllers/pig/personas_controller_spec.rb
+++ spec/controllers/pig/personas_controller_spec.rb
@@ -2,13 +2,28 @@ require 'rails_helper'
 
 module Pig
   RSpec.describe PersonasController, type: :controller do
-
     routes { Pig::Engine.routes }
 
-    describe "GET #index" do
-      it "returns http success" do
-        get :index
-        expect(response).to have_http_status(:success)
+    context "not signed in" do
+      requests = ['index']
+
+      let(:index) { get :index }
+
+      requests.each do |request|
+        it "should redirect #{request} to sign in" do
+          eval(request)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
+
+    context "signed in" do
+      login_admin
+      describe "GET #index" do
+        it "returns http success" do
+          get :index
+          expect(response).to have_http_status(:success)
+        end
       end
     end
 
