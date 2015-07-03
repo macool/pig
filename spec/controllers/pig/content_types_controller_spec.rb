3ba8fe050468c493c03b7d766cf60d91bdb21b53
Greg Woodcock <greg@yoomee.com>
2015-07-03 17:00:24 +0100

    Use the proper route for the content type reorder page

---
+++ spec/controllers/pig/content_types_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+module Pig
+  RSpec.describe ContentTypesController, type: :controller do
+    render_views
+    routes { Pig::Engine.routes }
+
+    context "not signed in" do
+      requests = ['index']
+
+      let(:index) { get :index }
+
+      requests.each do |request|
+        it "should redirect #{request} to sign in" do
+          eval(request)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+      end
+    end
+
+    context "signed in" do
+      login_admin
+
+      describe "GET #index" do
+        it "returns http success" do
+          get :index
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      describe "GET #reorder" do
+        let(:content_type) { FactoryGirl.create :content_type}
+        it "returns http success" do
+          get :reorder, id: content_type.id
+          expect(response).to have_http_status(:success)
+        end
+      end
+    end
+
+  end
+end
