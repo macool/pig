06891e6a744f37787f949a27ac1510371ca73bfc
Greg Woodcock <greg@yoomee.com>
2015-07-03 12:07:16 +0100

    Always render the pig application layout for pig views

--- app/controllers/pig/application_controller.rb
+++ app/controllers/pig/application_controller.rb
@@ -1,6 +1,8 @@
 module Pig
   class ApplicationController < ActionController::Base
 
+    layout "pig/application"
+
     rescue_from CanCan::AccessDenied do |exception|
       redirect_to pig.new_user_session_path
     end
