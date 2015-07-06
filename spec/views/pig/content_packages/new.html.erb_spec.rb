5ca9084eda174a845754b98135c52f8051cbd66f
Greg Woodcock <greg@yoomee.com>
2015-07-06 11:41:47 +0100

    Order author by last name on create form

--- spec/views/pig/content_packages/new.html.erb_spec.rb
+++ spec/views/pig/content_packages/new.html.erb_spec.rb
@@ -12,5 +12,16 @@ module Pig
         .xpath("//select[@id='content_package_requested_by_id']").text
       expect(assigned_to_options).to eq("\nA A\nB B\nC C")
     end
+
+    it "should order author surname, regardless of role" do
+      user_c = FactoryGirl.create(:user, :admin, last_name: 'C', first_name: 'C')
+      user_b = FactoryGirl.create(:user, :admin, last_name: 'B', first_name: 'B')
+      user_a = FactoryGirl.create(:user, :admin, last_name: 'A', first_name: 'A')
+      assign(:content_package, Pig::ContentPackage.new)
+      render
+      assigned_to_options = Nokogiri::HTML(rendered)
+        .xpath("//select[@id='content_package_author_id']").text
+      expect(assigned_to_options).to eq("\nA A (admin)\nB B (admin)\nC C (admin)")
+    end
   end
 end
