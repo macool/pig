8508a020d68d6cd4563d843f9cf5a32c4d73e7c8
Greg Woodcock <greg@yoomee.com>
2015-07-06 15:48:01 +0100

    Properly display the users name on user#show

--- app/cells/pig/user_cell.rb
+++ app/cells/pig/user_cell.rb
@@ -1,4 +1,15 @@
-class UserCell < Cell::ViewModel
+module Pig
+  class UserCell < Pig::Cell
+    property :first_name, :last_name
+    self.view_paths = ["#{Pig::Engine.root}/app/cells"]
 
+    def show
+      render
+    end
 
+    private
+    def full_name
+      "#{first_name} #{last_name}"
+    end
+  end
 end
--- spec/views/pig/manage/users/show.html.haml_spec.rb
+++ spec/views/pig/manage/users/show.html.haml_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 module Pig
   RSpec.describe 'pig/manage/users/show', type: :view do
-    it "should content authored by the user" do
-      user = FactoryGirl.create(:user, :author)
-      content_package = FactoryGirl.create(:content_package, author: user)
+    let(:user) { FactoryGirl.create(:user, :author, first_name: "Jimmy",
+                                                    last_name: "Foo") }
+    let(:content_package) { FactoryGirl.create(:content_package, author: user) }
+
+    before(:each) do
       assign(:user, user)
       assign(:assigned_content, [content_package])
       allow(view).to receive(:can?).and_return(true)
       render
+    end
+
+    it "should show the name of the user" do
+      expect(rendered).to have_text("Jimmy Foo")
+    end
+
+    it "should show content authored by the user" do
       expect(rendered).to have_content(content_package.name)
     end
   end
