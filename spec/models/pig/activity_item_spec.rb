015c4377902aa3ab3f8f745d75e6d90e66ca76a3
Greg Woodcock <greg@yoomee.com>
2015-07-08 16:07:21 +0100

    Add model tests for activity item

---
+++ spec/models/pig/activity_item_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+module Pig
+  RSpec.describe ActivityItem, type: :model do
+
+    it { should belong_to(:user) }
+    it { should belong_to(:resource) }
+    it { should belong_to(:parent_resource) }
+
+    it { should validate_presence_of :user }
+    
+    describe 'default scope' do
+      let!(:content_package_1) { FactoryGirl.create(:content_package) }
+      let!(:content_package_2) { FactoryGirl.create(:content_package) }
+
+      it 'orders by created_at descending' do
+        ActivityItem.all.should eq [content_package_2.activity_items.first, content_package_1.activity_items.first]
+      end
+    end
+
+  end
+end
