387dc1d876d500e26900764d976d2487eee3bb49
Nick Bolt <nick@yoomee.com>
2015-08-10 15:26:39 +0100

    fix validate method call from block format

--- app/models/pig/redactor_image_upload.rb
+++ app/models/pig/redactor_image_upload.rb
@@ -3,7 +3,7 @@ module Pig
 
     dragonfly_accessor :file
     validates_property :format, :of => :image, :in => [:jpeg, :jpg, :png, :gif, :JPEG, :JPG, :PNG, :GIF], :message => "must be an image"
-    validate :file_uid, :presence => true
+    validates :file_uid, :presence => true
 
     def image
       file_type == 'image' ? file : nil
