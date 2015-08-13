f151472dff33e811120172ca010ecac67e401e89
Jon Kyte <jon@yoomee.com>
2015-08-13 12:37:43 +0100

    style image alt text input

--- app/inputs/image_input.rb
+++ app/inputs/image_input.rb
@@ -6,6 +6,7 @@ class ImageInput < FormtasticBootstrap::Inputs::FileInput
       if object.send(input_name) && object.errors[input_name].blank?
         out << template.image_for(object, '60x60#', :method => input_name)
       end
+      out << '<div class="row"><div class="col-sm-6">'
       out << '<div class="image-inputs">'
       out << builder.file_field(method, input_html_options)
       if object.send(input_name).present?
@@ -13,13 +14,11 @@ class ImageInput < FormtasticBootstrap::Inputs::FileInput
           "<input type='checkbox' name='content_package[remove_#{input_name}]' value='1'></input>".html_safe + " Remove #{label_text.downcase.gsub('<abbr title="required">*</abbr>', '')}"
         end
       end
-      out << '</div>'
+      out << '</div></div><div class="col-sm-6 short-input image-alt-container-field">'
       if object.respond_to?("#{input_name}_alt")
-        out << template.content_tag(:label, :class => 'alt-tag') do
-          "#{input_name.to_s.humanize} Alt Text"
-        end
-        out << builder.text_field("#{input_name}_alt")
+        out << builder.text_field("#{input_name}_alt", :placeholder => "Image alt text")
       end
+      out << '</div></div>'
     end << '<div class="clearfix"></div>'.html_safe
   end
 
