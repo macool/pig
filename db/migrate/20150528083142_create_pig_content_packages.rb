b2789d88c2d04a9889864d1c387042abb1c25289
Greg Woodcock <greg@yoomee.com>
2015-08-18 12:05:16 +0100

    Add Awesome nested set for defining a tree structure on content packages

--- db/migrate/20150528083142_create_pig_content_packages.rb
+++ db/migrate/20150528083142_create_pig_content_packages.rb
@@ -5,13 +5,15 @@ class CreatePigContentPackages < ActiveRecord::Migration
       t.string :slug
       t.string :name
       t.belongs_to :content_type
-      t.integer :position, :default => 0
-      t.belongs_to :parent
+      t.integer :position, default: 0
+      t.integer :parent_id, null: true, index: true
+      t.integer :lft, null: false, index: true
+      t.integer :rgt, null: false, index: true
       t.belongs_to :author
       t.belongs_to :requested_by
-      t.string :status, :default => 'draft'
-      t.boolean :logged_in_only, :default => false
-      t.boolean :hide_from_robots, :default => false
+      t.string :status, default: 'draft'
+      t.boolean :logged_in_only, default: false
+      t.boolean :hide_from_robots, default: false
       t.text :notes
       t.date :due_date
       t.integer :review_frequency
@@ -25,11 +27,10 @@ class CreatePigContentPackages < ActiveRecord::Migration
       t.string :meta_image_uid
       t.string :meta_image_name
       t.json :json_content, null: false, default: '{}'
-
-
+      t.integer :depth, :null => false, :default => 0
+      t.integer :children_count, :null => false, :default => 0
       t.timestamps
     end
-    add_index :pig_content_packages, :parent_id
     add_index :pig_content_packages, :slug
   end
 
