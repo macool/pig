03c6bc6e8e23daaf22fc9aa32cdf8143e1da7207
Greg Woodcock <greg@yoomee.com>
2016-01-14 11:23:15 +0000

    Automatically create tag category slugs
    On Tag category creation derive a slug from the name

--- spec/views/pig/tag_categories/_form.html.haml_spec.rb
+++ spec/views/pig/tag_categories/_form.html.haml_spec.rb
@@ -12,10 +12,6 @@ RSpec.describe "pig/admin/tag_categories/_form", type: :view do
     expect(rendered).to have_xpath("//input[@name='tag_category[name]']")
   end
 
-  it "should have a slug field" do
-    expect(rendered).to have_xpath("//input[@name='tag_category[slug]']")
-  end
-
   it "should have a name field" do
     expect(rendered).to have_xpath("//input[@name='tag_category[taxonomy_list][]']")
   end
