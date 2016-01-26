f30848cbf0c47ac550717c49eea1eb6a2d519b5c
Greg Woodcock <greg@yoomee.com>
2016-01-26 14:33:03 +0000

    When publishing a content package email the author to let them know

--- app/mailers/pig/content_package_mailer.rb
+++ app/mailers/pig/content_package_mailer.rb
@@ -3,14 +3,20 @@ module Pig
 
     helper Pig::UrlHelper
 
-    default :from => "\"#{Settings.site_name}\" <#{Settings.site_noreply_email}>"
+    default from: "\"#{Settings.site_name}\" <#{Settings.site_noreply_email}>"
 
     def assigned(content_package, assigned_to)
       @content_package = content_package
       @user = assigned_to
       reason = @content_package.author.present? ? 'writing' : 'approval'
       @content = "has being assigned to you for #{reason}"
-      mail(:to => assigned_to.email, :subject => "[#{Settings.site_name}] #{content_package.name} #{@content}" )
+      mail(to: assigned_to.email, subject: "[#{Settings.site_name}] #{content_package.name} #{@content}")
+    end
+
+    def published(content_package, last_edited_by)
+      @content_package = content_package
+      @user = last_edited_by
+      mail(to: last_edited_by.email, subject: "[#{Settings.site_name}] #{content_package.name} has been published")
     end
 
   end
