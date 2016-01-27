8b27ce6414a5d2ed9ed5904f13cfb53532437904
Greg Woodcock <greg@yoomee.com>
2016-01-27 09:35:10 +0000

    Add rake task to email editors of content getting old

--- app/mailers/pig/content_package_mailer.rb
+++ app/mailers/pig/content_package_mailer.rb
@@ -19,5 +19,16 @@ module Pig
       mail(to: last_edited_by.email, subject: "[#{Settings.site_name}] #{content_package.name} has been published")
     end
 
+    def getting_old(user, content_packages)
+      @content_packages = content_packages
+      @user = user
+      if @content_packages.count > 1
+        title = "#{@content_packages.count} content packages are "
+      else
+        title = "#{@content_packages.first.name} is"
+      end
+      mail(to: @user.email, subject: "[#{Settings.site_name}] #{title} getting old")
+    end
+
   end
 end
