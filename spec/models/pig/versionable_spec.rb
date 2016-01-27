50f5f08842e37923b39343eb2420ee80fe49fde1
Ant Barnes <ant@yoomee.com>
2016-01-27 13:53:13 +0000

    Add live version method

--- spec/models/pig/versionable_spec.rb
+++ spec/models/pig/versionable_spec.rb
@@ -3,13 +3,53 @@ require 'rails_helper'
 module Pig
   RSpec.describe ContentPackage do
 
+    let(:content_package) do
+      FactoryGirl.create(:content_package,
+                       status: 'draft',
+                        name: 'Test name')
+    end
+
     describe 'creating new versions' do
-      context 'when changing status to published' do
+      context 'when changing status from published' do
         it 'it creates a new version' do
-          cp = ContentPackage.create(status: "draft")
-          cp.status = 'published'
-          cp.save
-          expect(cp.versions.count).to eq(1)
+          content_package.update_attributes status: 'published'
+          expect(content_package.versions.count).to eq(0)
+
+          content_package.update_attributes status: 'draft'
+          expect(content_package.versions.count).to eq(1)
+        end
+      end
+
+      context 'when changing status to pending' do
+        it 'it does not create a new version' do
+          content_package.update_attributes status: 'pending'
+          expect(content_package.versions.count).to eq(0)
+        end
+      end
+
+      context 'when changing status to draft' do
+        it 'it does not create a new version' do
+          content_package.update_attributes status: 'draft'
+          expect(content_package.versions.count).to eq(0)
+        end
+      end
+    end
+
+    describe 'getting live version' do
+      context 'when there is no published version' do
+        it 'should return the content package' do
+          expect(content_package.live_version).to equal(content_package)
+        end
+      end
+
+      context 'when there is a published version' do
+        before do
+          content_package.update_attributes status: 'published', name: 'Published name'
+          content_package.update_attributes name: 'Changed name', status: 'draft'
+        end
+
+        it 'should return the published version' do
+          expect(content_package.live_version.name).to eq('Published name')
         end
       end
     end
