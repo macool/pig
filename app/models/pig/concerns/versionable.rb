26671ddfbc7fcba826405219082bef137bfbbf6b
Ant Barnes <ant@yoomee.com>
2016-01-27 13:53:13 +0000

    Add live version method

--- app/models/pig/concerns/versionable.rb
+++ app/models/pig/concerns/versionable.rb
@@ -5,7 +5,15 @@ module Pig
 
       included do
         has_paper_trail class_name: 'Pig::Version',
-                        if: proc { |t| t.status == 'published' }
+                        if: proc { |t| t.status_was == 'published' }
+      end
+
+      def live_version
+
+        if self.versions.any?
+          return self.versions.last.reify
+        end
+        self
       end
 
     end
