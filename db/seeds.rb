965ad21c604d49d3d2759b08613d296f77641185
Daniel King <dan@llamadigital.net>
2016-03-24 15:24:24 +0000

    add required due_date to content package seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ u.confirm
 ct = Pig::ContentType.create!(name: "home", view_name: "homepage")
 Pig::ContentPackage.create!(slug: "home", name: "home", content_type: ct,
                             editing_user: u, requested_by: u, next_review: Date.today,
+                            due_date: Date.today,
                             position: 0, author: u, status: "draft", meta_title: "home",
                             json_content: {"content_chunks"=>{"title"=>{"value"=>"home page", "field_type"=>"string"}}})
 Pig::ContentAttribute.create!(content_type: ct, slug: "title", name: "title",
