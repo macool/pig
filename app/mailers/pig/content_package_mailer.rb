890306ca51a1a8884d1f95a2e34f4a5f29fcd9d1
Greg Woodcock <greg@yoomee.com>
2016-08-15 17:04:14 +0100

    Move assigned mailers to separate methods to allow easier overriding in main app

--- app/mailers/pig/content_package_mailer.rb
+++ app/mailers/pig/content_package_mailer.rb
@@ -1,20 +1,19 @@
 module Pig
   class ContentPackageMailer < ActionMailer::Base
-
     helper Pig::UrlHelper
 
     default from: Settings.site_noreply_email
 
-    def assigned(content_package, assigned_to, reason)
-      @content_package = content_package
-      @user = assigned_to
-      @content = "has being assigned to you for #{reason}"
-      mail(to: assigned_to.email,
-           subject: default_i18n_subject(
-             site_name: Settings.site_name,
-             content_package_name: content_package.name,
-             content: @content
-           ))
+    def assigned_for_review(content_package, assigned_to)
+      send_assigned_email(content_package, assigned_to)
+    end
+
+    def assigned_for_writing(content_package, assigned_to)
+      send_assigned_email(content_package, assigned_to)
+    end
+
+    def assigned_for_updating(content_package, assigned_to)
+      send_assigned_email(content_package, assigned_to)
     end
 
     def published(content_package, last_edited_by)
@@ -39,6 +38,18 @@ module Pig
            subject: default_i18n_subject(
              site_name: Settings.site_name,
              title: title
+           ))
+    end
+
+    private
+
+    def send_assigned_email(content_package, to)
+      @content_package = content_package
+      @user = to
+      mail(to: to.email,
+           subject: default_i18n_subject(
+             site_name: Settings.site_name,
+             content_package_name: content_package.name
            ))
     end
   end
