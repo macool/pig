da06e8a7a668f9c309c53a0e968e18fe68427c54
Nick Bolt <nick@yoomee.com>
2016-09-27 10:56:29 +0100

    Move helper requirements into controllers which don't inherit from pig

--- app/controllers/pig/admin/sessions_controller.rb
+++ app/controllers/pig/admin/sessions_controller.rb
@@ -2,6 +2,14 @@ module Pig
   module Admin
     class SessionsController < Devise::SessionsController
       layout 'pig/simple'
+
+      helper Pig::ApplicationHelper
+      helper Pig::MetaTagsHelper
+      helper Pig::LayoutHelper
+      helper Pig::NavigationHelper
+      helper Pig::ContentHelper
+      helper Pig::TitleHelper
+      helper Pig::ImageHelper
     end
   end
 end
